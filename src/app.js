d905210f943c69b54d68256a1e21380b21ffc114
dowonee <jjddwwyt@gmail.com>
2025-04-09 01:11:11 +0900

    feat: HTTP 상태 코드 유틸 정의

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import createError from "http-errors";
+
+import httpStatusCode from "./utils/httpStatusCode.js";
 import connectDatabase from "./config/database.js";
 
 connectDatabase();
@@ -18,15 +20,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 app.use((req, res, next) => {
-  next(createError(404));
+  next(createError(httpStatusCode.NOT_FOUND));
 });
 
 app.use((err, req, res) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || httpStatusCode.INTERNAL_SERVER_ERROR);
 });
 
 export default app;
