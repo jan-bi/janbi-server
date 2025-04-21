6a3d0fd8aac571d81ea784b4f352637f7db3e8d5
dowonee <jjddwwyt@gmail.com>
2025-04-21 17:59:27 +0900

    feat: ChangeLog 스키마 추가 (#7)

---
+++ src/models/ChangeLog.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const changeLogSchema = new mongoose.Schema({
+  urlId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Url",
+    required: true
+  },
+  scheduledTime: {
+    type: Date,
+    required: true
+  },
+  isChanged: {
+    type: Boolean,
+    default: false
+  },
+  changedSelectors: {
+    type: [String],
+    default: []
+  },
+  alreadyNotified: {
+    type: Boolean,
+    default: false
+  },
+});
+
+export default mongoose.model("ChangeLog", changeLogSchema);
