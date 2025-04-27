ce50be88c676c37a57b2ed9ead1e95599b91cee5
dowonee <jjddwwyt@gmail.com>
2025-04-27 20:02:37 +0900

    refactor: Slack 알림 조건 수정 및 메시지 수정

--- src/services/slackAlert.js
+++ src/services/slackAlert.js
@@ -1,17 +1,17 @@
 export default async function sendSlackAlert({ webhookUrl, name, changes }) {
   if (!webhookUrl) return;
 
-  let text = "";
+  const changed = changes.some(change => change.beforeHtml !== change.afterHtml);
 
-  if (changes && changes.length > 0) {
-    text =   `
-    ${name} 페이지에서 모니터링 중인 요소가 변경되었습니다.\n\n` +
-    changes.map((change) => {
-      return `모니터링 요소: ${change.selector}\n 이전 값: ${change.before}\n 변경된 값: ${change.after}`;
-    });
-  } else {
-    text = `${name} 페이지의 모니터링 요소에 변경 사항이 없습니다.`;
-  }
+  let text = changed
+    ? `${name} 페이지에서 모니터링 중인 요소가 변경되었습니다.\n\n`
+    : `${name} 페이지의 모니터링 요소에 변경 사항이 없습니다.\n\n`;
+
+  text += changes
+    .map(change => {
+      return `이전 값: ${change.beforeHtml}\n변경된 값: ${change.afterHtml}`;
+    })
+    .join("\n\n");
 
   try {
     await fetch(webhookUrl, {
