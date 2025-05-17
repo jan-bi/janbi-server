7b699a34f23f45416e1701242bee68ee16cf31c4
dowonee <jjddwwyt@gmail.com>
2025-05-17 16:26:32 +0900

    refactor: 로그인한 사용자 정보를 req.user에 담도록 수정

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -9,8 +9,13 @@ export async function authenticate(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id);
+    const user = await User.findById(decoded.id);
 
+    if (!user) {
+      return res.status(httpStatusCode.UNAUTHORIZED).json({ message: "사용자를 찾을 수 없습니다." });
+    }
+
+    req.user = user;
     next();
   } catch {
     res.status(httpStatusCode.UNAUTHORIZED).json({ message: "토큰이 유효하지 않습니다" });
