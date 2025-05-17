d6295571555cc4a7c1fb469a2c2932570419254d
dowonee <jjddwwyt@gmail.com>
2025-05-17 16:27:20 +0900

    feat: URL 요청에 로그인 상태 확인하는 미들웨어 추가

--- src/routes/urls.js
+++ src/routes/urls.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { addUrl, getAllUrls, getUrlHistory } from "../controllers/urlController.js";
+import { authenticate } from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
+
+router.use(authenticate);
 
 router.post("/", addUrl);
 router.get("/", getAllUrls);
