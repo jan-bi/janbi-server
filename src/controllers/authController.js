f2c0b541cd0278e85532e667bccb15b58a004aa6
dowonee <jjddwwyt@gmail.com>
2025-05-19 08:23:45 +0900

    refactor: 배포 환경에서 쿠키 전송을 위해 쿠키 설정 수정

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,20 +3,21 @@ import User from "../models/User.js";
 
 export async function handleGoogleCallback(req, res) {
   const { accessToken, refreshToken } = generateTokens(req.user);
+  const isProd = process.env.NODE_ENV === "production";
 
   await User.findByIdAndUpdate(req.user._id, { refreshToken });
 
   res
     .cookie("accessToken", accessToken, {
-      httpOnly: process.env.NODE_ENV === "production",
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      httpOnly: true,
+      secure: isProd,
+      sameSite: isProd ? "None" : "Lax",
       maxAge: 60 * 60 * 1000,
     })
     .cookie("refreshToken", refreshToken, {
-      httpOnly: process.env.NODE_ENV === "production",
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      httpOnly: true,
+      secure: isProd,
+      sameSite: isProd ? "None" : "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
     .redirect(`${process.env.CLIENT_ORIGIN}/auth/success`);
