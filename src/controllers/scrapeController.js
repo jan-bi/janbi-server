db25f70e3684b92a3bc62e12f6773a9f91b22134
dowonee <jjddwwyt@gmail.com>
2025-06-04 23:13:08 +0900

    refactor: 스크래핑 시 URL 대신 ID 기반으로 조회하도록 수정

--- src/controllers/scrapeController.js
+++ src/controllers/scrapeController.js
@@ -2,26 +2,44 @@ import "dotenv/config.js";
 import httpStatusCode from "../utils/httpStatusCode.js";
 import scrapePage from "../services/pageScraper.js";
 import Url from "../models/Url.js";
+import ChangeLog from "../models/ChangeLog.js";
 
-export const getPageHtml = async (req, res) => {
-  const { url } = req.body;
+export const scrapeUrlById = async (req, res) => {
+  const { id } = req.params;
 
-  if (!url) {
-    return res.status(httpStatusCode.BAD_REQUEST).json({ message: "분석할 URL이 없습니다." });
-  }
+  try {
+    const savedUrl = await Url.findById(id);
 
-  const scrapeResult = await scrapePage(url);
-
-  if (scrapeResult.success) {
-    const savedUrl = await Url.findOne({ url });
-
-    if (savedUrl) {
-      savedUrl.previousHtml = scrapeResult.data.fullHtml;
-      await savedUrl.save();
+    if (!savedUrl) {
+      return res.status(httpStatusCode.NOT_FOUND).json({ message: "해당 ID의 URL을 찾을 수 없습니다." });
     }
 
-    return res.status(httpStatusCode.OK).json(scrapeResult);
-  } else {
-    return res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json(scrapeResult);
+    if (!savedUrl.userId.equals(req.user._id)) {
+      return res.status(httpStatusCode.UNAUTHORIZED).json({ message: "권한이 없습니다." });
+    }
+
+
+    const scrapeResult = await scrapePage(savedUrl.url, savedUrl.selectors);
+
+    if (scrapeResult.success) {
+      savedUrl.previousHtml = scrapeResult.data.fullHtml;
+      await savedUrl.save();
+
+      await ChangeLog.create({
+        urlId: savedUrl._id,
+        scheduledTime: new Date(),
+        isChanged: true,
+        changedSelectors: [],
+        changedContents: scrapeResult.data,
+        alreadyNotified: false,
+      });
+
+      return res.status(httpStatusCode.OK).json(scrapeResult);
+    } else {
+      return res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json(scrapeResult);
+    }
+  } catch (err) {
+    console.error("스크래핑 중 오류 발생:", err);
+    return res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({ message: "서버 오류가 발생했습니다." });
   }
 };
